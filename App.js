cd00f04ef5d12ff9cb42086f152f84ce39726ac7
shrCreation <shrhossain786@gmail.com>
2023-02-01 15:54:08 +0600

    add youtube video and check record

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import YoutubePlayer from 'react-native-youtube-iframe';
 
 const App = () => {
+  // const [playing, setPlaying] = useState(false);
+  // const togglePlaying = () => {
+  //   setPlaying(true);
+  // };
   return (
-    <SafeAreaView style={{alignItems: 'center', justifyContent: 'center'}}>
-      <TouchableOpacity>
-        <Text>App Screenshot Test</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={{alignItems: 'center', marginTop: 40, flex: 1}}>
+      <Text>App Screen Control</Text>
+      <Text style={{marginTop: 20}}>Youtube Video</Text>
+      <YoutubePlayer
+        width={400}
+        height={400}
+        play={true}
+        videoId={'gvkqT_Uoahw'}
+      />
+      {/* <TouchableOpacity onPress={togglePlaying}>
+        <Text>{playing ? 'Pause' : 'Play'}</Text>
+      </TouchableOpacity> */}
     </SafeAreaView>
   );
 };
